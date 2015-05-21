e2a55ee7669b5af87a250d4cd1ad4bb39fcf83a9
Your Name Here <ornacium@gmail.com>
2015-05-21 15:00:26 -0400

    Fixed dialog for currency formatting and input.

--- app/src/main/java/com/example/thomasherring/receiptrocket/AddReceiptPage.java
+++ app/src/main/java/com/example/thomasherring/receiptrocket/AddReceiptPage.java
@@ -9,8 +9,11 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,6 +24,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import objects.Receipt;
+
+import java.text.NumberFormat;
 import java.util.Calendar;
 
 
@@ -33,44 +38,21 @@ public class AddReceiptPage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_receipt_page);
+        receiptValue = 00.00;
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+
     protected void onResume(){
         super.onResume();
-        final AlertDialog.Builder keypadBuilder = new  AlertDialog.Builder(this);
         getDialogEnteredValue();
 
-//        keypadBuilder.setCancelable(true);
-//        keypadBuilder.setView(R.layout.receipt_enter_keypad);
-//
-//        keypadBuilder.setNegativeButton("Cancel",
-//                new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int id) {
-//                        dialog.cancel();
-//                        Intent intent = new Intent();
-//                        setResult(RESULT_CANCELED, intent);
-//                        finish();
-//                    }
-//                });
-//
-//        //Add amount to current receipt
-//        keypadBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-//            public void onClick(DialogInterface dialog, int id) {
-//                Dialog d = (Dialog) dialog;
-//                TextView keypadValue = (TextView)d.findViewById(R.id.keypadText);
-//                receiptValue = Double.parseDouble(keypadValue.getText().toString());
-//                dialog.cancel();
-//            }
-//        });
-//
-//
-//
-//        AlertDialog keypad = keypadBuilder.create();
-//        keypad.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
-//        keypad.show();
 
     }
+
+    public void updateValue(View view){
+        getDialogEnteredValue();
+    }
+
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public void getDialogEnteredValue(){
@@ -78,34 +60,68 @@ public class AddReceiptPage extends Activity {
 
         final AlertDialog.Builder keypadBuilder = new  AlertDialog.Builder(this);
         keypadBuilder.setCancelable(true);
-        keypadBuilder.setView(R.layout.receipt_enter_keypad);
+
+
+        LayoutInflater factory = LayoutInflater.from(AddReceiptPage.this);
+        View dialogContent = factory.inflate(R.layout.receipt_enter_keypad, null);
+
+        final EditText keypadValue = (EditText)dialogContent.findViewById(R.id.keypadText);
+        keypadValue.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            private String current = "";
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    keypadValue.removeTextChangedListener(this);
+
+                    String cleanString = s.toString().replaceAll("[$,.]", "");
+
+                    double parsed = Double.parseDouble(cleanString);
+                    String formatted = NumberFormat.getCurrencyInstance().format((parsed / 100));
+
+                    current = formatted;
+                    keypadValue.setText(formatted);
+                    keypadValue.setSelection(formatted.length());
+                    keypadValue.addTextChangedListener(this);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        keypadBuilder.setView(dialogContent);
 
         keypadBuilder.setNegativeButton("Cancel",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         dialog.cancel();
-                        Intent intent = new Intent();
-                        setResult(RESULT_CANCELED, intent);
-                        finish();
+//                        Intent intent = new Intent();
+//                        setResult(RESULT_CANCELED, intent);
+//                        finish();
                     }
                 });
 
         //Add amount to current receipt
         keypadBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-                Dialog d = (Dialog) dialog;
-                TextView keypadValue = (TextView)d.findViewById(R.id.keypadText);
-                receiptValue = Double.parseDouble(keypadValue.getText().toString());
+                //Dialog d = (Dialog) dialog;
+                if(keypadValue.getText().toString().length() > 0) {
+                    receiptValue = Double.parseDouble(keypadValue.getText().toString().substring(1));
+                }
                 dialog.cancel();
             }
         });
 
-
-
         AlertDialog keypad = keypadBuilder.create();
         keypad.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
-        keypad.show();
 
+        keypad.show();
     }
 
 
